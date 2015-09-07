c13949be9eaf4d10c4ead3b528fbe8d17d1b6411
Raul Erkki Hirvelä <raulhir@greeny.cs.tlu.ee>
2015-09-07 14:40:09 +0300

    lisatud kuupaev

--- helloworld.php
+++ helloworld.php
@@ -4,6 +4,42 @@ $first_name = "raul";
 $last_name = "hirvela";
 
 //trükib välja
-:echo $first_name." ".$last_name;
+echo $first_name." ".$last_name;
 
 ?>
+
+<br>
+
+<?php
+
+	$age = 21;
+	
+	// if( loogikatehe ) { tõene } else { väär }
+	if($age < 18) {
+		echo "alaealine";
+	} else {
+		echo "täisealine";
+	}
+	
+	?>
+	
+	<br>
+	
+	<?php
+	
+		for($i = 0; $i < $age; $i = $i + 1) {
+			//see, mida korratakse
+			echo "palju,".$i.", ";
+			}
+		
+		echo "õnne";
+	
+	?>
+	
+	<br>
+	
+	<?php
+	
+	//kuupäev - date
+	echo date ("l, j F Y e")
+	?>
